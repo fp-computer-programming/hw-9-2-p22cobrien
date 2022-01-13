3aa828d506dcaf18d2bdace65a30083e0afb9588
p22cobrien <p22cobrien@fairfieldprep.org>
2022-01-13 10:19:01 -0500

    submit hw9-2-1.py

--- hw9-2-1.py
+++ hw9-2-1.py
@@ -1,9 +1,17 @@
 # Author: CMOB 1/12/2022
 
-rows = [["Mateo", "Jason", "Jordan", "Mohamed", "Michael", "Charlie", "Declan"], ["Sean", "Luis", "Ryan", "James", "Jack"], ["Aiden", "Ian", "Colin" "Tim", "Cam"], ["Larry", "Spencer", "Chris", "Ryan", "Nolan"]]
-last_initials = [["B.","D.","H.","E.","G.","G.","H."],["R.","M.","L.","I.","I."],["N.","N.","O.","P.","P."],["X.","T.","S.","S.","P."]]
-x = 0
+from ast import Delete
+from email.encoders import encode_noop
+
+
+rows = [["Mateo", "Jason", "Jordan", "Mohamed", "Michael", "Charlie", "Declan"], ["Sean", "Luis", "Ryan", "James", "Jack"], ["Aiden", "Ian", "Colin","Tim", "Cam"], ["Larry", "Spencer", "Chris", "Ryan", "Nolan"]]
+last_initials = ["B.","D.","H.","E.","G.","G.","H.","R.","M.","L.","I.","I.","N.","N.","O.","P.","P.","X.","T.","S.","S.","P."]
+
 for row in rows:
-    for names in rows:
-        names = names + last_initials[x]
-        x += 1
+    for index, value in enumerate(row):
+        row[index] = "{0} {1}".format(value,last_initials[0])
+        del last_initials[0]
+
+
+print(rows)
+
