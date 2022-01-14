f3482b6259d92ffdb09fd3ce9a7920faae865c95
p22cobrien <p22cobrien@fairfieldprep.org>
2022-01-14 09:49:53 -0500

    remove random modules

--- hw9-2-1.py
+++ hw9-2-1.py
@@ -1,9 +1,5 @@
 # Author: CMOB 1/12/2022
 
-#from ast import Delete
-#from email.encoders import encode_noop
-
-
 rows = [["Mateo", "Jason", "Jordan", "Mohamed", "Michael", "Charlie", "Declan"], ["Sean", "Luis", "Ryan", "James", "Jack"], ["Aiden", "Ian", "Colin","Tim", "Cam"], ["Larry", "Spencer", "Chris", "Ryan", "Nolan"]]
 last_initials = ["B.","D.","H.","E.","G.","G.","H.","R.","M.","L.","I.","I.","N.","N.","O.","P.","P.","X.","T.","S.","S.","P."]
 
